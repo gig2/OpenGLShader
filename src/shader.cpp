4556fc48218cc7c92f682bfd44efb5b68ce6dbdc
Thibault Payet <monwarez@gmail.com>
2018-11-24 08:39:56 +0100

    Revert "add some log, bind frag datalocation to 0 with out_Color"
    This reverts commit 2bc4ece1e20d02ed2c4205ec5a7d42e65c7079e3.

--- src/shader.cpp
+++ src/shader.cpp
@@ -26,11 +26,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include "shader.h"
 
-#include <array>
-#include <iostream>
 #include <sstream>
 #include <vector>
-
 using namespace S3DE;
 // Constructeurs et Destructeur
 
@@ -149,10 +146,6 @@ void Shader::Load()
         if ( !retF )
         {
             throw std::string{"Error during build fragment shader"};
-        }
-        else
-        {
-            glBindFragDataLocation( m_fragmentID, 0, "out_Color" );
         }
     }
 
@@ -303,7 +296,7 @@ bool Shader::BuildShader( GLuint &shader, GLenum type, std::string const &source
         glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &errorSize );
 
         // Allocation de m?moire
-        std::vector<GLchar> perror;
+        std::vector<char> perror;
         perror.resize( errorSize );
 
         glGetShaderInfoLog( shader, errorSize, &errorSize, perror.data() );
@@ -320,15 +313,7 @@ bool Shader::BuildShader( GLuint &shader, GLenum type, std::string const &source
 
         return false;
     }
+
     else
-    {
-        std::size_t const logSize{512};
-        std::array<GLchar, logSize> log;
-
-        glGetShaderInfoLog( shader, logSize, nullptr, log.data() );
-
-        std::cout << "Shader build log:\n" << log.data() << "\n";
-
         return true;
-    }
 }
